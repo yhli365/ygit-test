7c33b5d4e6996dba2961943ae63e823502bf31fb
yhli365 <yhli365@126.com>
2014-05-11 14:47:31 +0800

    java test: print current date

---
+++ src/java/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Current Time: " + new Date());
+	}
+
+}
